b692abd75113a8c9b40632290a143feef27a9653
charlie <cburholt@icloud.com>
2023-11-09 00:04:17 +0000

    don't use circular buffer
    since we don't want to overwrite old nodes

--- search.c
+++ search.c
@@ -2,7 +2,6 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-#define BUF_SIZE 5012
 
 typedef struct Node Node;
 struct Node {
@@ -11,24 +10,25 @@ struct Node {
 	Node *prev; // NOTE if searching is still slow, get rid of this feild
 };
 
+#define QUEUE_SIZE 25'000
 typedef struct Queue Queue;
 struct Queue {
 	int front, back;
-	Node buf[BUF_SIZE];	// check if this is a good size
+	Node buf[QUEUE_SIZE];
 };
 
 static void queue_push(Queue *q, Node *n)
 {
-	assert((q->front + 1) % BUF_SIZE != q->back);
+	assert(q->front < QUEUE_SIZE);
 	q->buf[q->front] = *n;
-	q->front = (1 + q->front) % BUF_SIZE;
+	++q->front;
 }
 
 static Node *queue_pop(Queue *q)
 {
-	assert(q->back != q->front);
+	assert(q->back < q->front);
 	Node *n = &q->buf[q->back];
-	q->back = (q->back + 1) % BUF_SIZE;
+	++q->back;
 	return n;
 }
 
@@ -37,11 +37,12 @@ static bool queue_empty(Queue *q)
 	return q->front == q->back;
 }
 
+#define SET_SIZE 5'000
 // NOTE change this to a hash set if searching is slow
 typedef struct Set Set;
 struct Set {
 	int front;
-	Node buf[BUF_SIZE];
+	Node buf[SET_SIZE];
 };
 
 static bool set_contains(Set *s, Node *n)
@@ -55,7 +56,7 @@ static bool set_contains(Set *s, Node *n)
 
 static void set_push(Set *s, Node *n)
 {
-	assert(s->front < BUF_SIZE);
+	assert(s->front < SET_SIZE);
 	s->buf[s->front++] = *n;
 }
 
