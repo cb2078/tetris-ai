1dea1bd7e7a358b2e46416b9208dd99473b5d678
unknown <cb2078@york.ac.uk>
2023-12-22 20:18:40 +0000

    create build directory

--- build.bat
+++ build.bat
@@ -3,5 +3,8 @@
 :: suppress: global variable, zero sized array extension, brackets for precedence
 set CF=/nologo /Z7 /W4 /wd4459 /wd4200 /wd4554 /D_CRT_SECURE_NO_WARNINGS
 
-:: cl %CF% generator.c & generator
-cl %CF% /I"raylib\include" main.c /link /nodefaultlib:libcmt /libpath:"raylib\lib" msvcrt.lib raylib.lib opengl32.lib gdi32.lib winmm.lib kernel32.lib shell32.lib user32.lib
+if not exist build mkdir build
+pushd build
+:: cl %CF% ..\generator.c & generator
+cl %CF% /I"..\raylib\include" ..\main.c /link /nodefaultlib:libcmt /libpath:"..\raylib\lib" msvcrt.lib raylib.lib opengl32.lib gdi32.lib winmm.lib kernel32.lib shell32.lib user32.lib
+popd
