9a4d6247fa41c91c4372ad4fce7204cf6997e4e7
unknown <cb2078@york.ac.uk>
2024-07-30 14:23:46 +0100

    sort scores on chart and colour them with a gradient

--- graph.py
+++ graph.py
@@ -7,7 +7,7 @@ epochs = len(d)
 num_weights = len(d[0]['weights'])
 weights = [[e['weights'][i] for e in d] for i in range(num_weights)]
 population = len(d[0]['lines'])
-lines = [sorted(e['lines'][i] for e in d) for i in range(population)]
+lines = [[sorted(e['lines'])[i] for e in d] for i in range(population)]
 
 fig, (ax1, ax2) = plt.subplots(2, 1, sharex = True)
 # fig.subplots_adjust(hspace = 0)
@@ -18,9 +18,10 @@ for i in range(num_weights): ax1.plot(weights[i], color=colours[i], label='weigh
 ax1.legend()
 
 colour = 'blue'
+colours = [(0, 0, 1, 1 - i / population) for i in range(population)]
 ax2.set_xlabel('epoch')
 ax2.set_ylabel('lines')  # we already handled the x-label with ax1
-for i in range(population): ax2.plot(lines[i], color=colour)
+for i in range(population): ax2.plot(lines[i], color=colours[i])
 
 fig.tight_layout()  # otherwise the right y-label is slightly clipped
 plt.show()
